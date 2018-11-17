f7bcf71735c0c75557ced4a5a62d3810b1edf827
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2018-11-17 23:35:13 +0200

    Update Jump It.cpp

--- Jump It.cpp
+++ Jump It.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include "random"
+#include "time.h"
 using namespace std;
 
 int getMinCost(int* a, int size, int start, int cCost = 0){
@@ -11,13 +12,21 @@ int getMinCost(int* a, int size, int start, int cCost = 0){
     	return oneMove;
     }
     int twoMoves = getMinCost(a, size, start+2, cCost + a[start+2]);
-    //cout << oneMove << " " << twoMoves << endl;
+    ///cout << oneMove << " " << twoMoves << endl;
     return min(oneMove, twoMoves);
 }
 
 int main(){
-    int a[7] = {0, 10, 50, 100, 70, 8, 9};
-    int minCost = getMinCost(a, 7, 0);
-    cout << minCost << endl;
+    int n;
+    srand (time(NULL));
+    cout << "Enter number of cols: ";
+    cin >> n;
+    int *bar = new int[n];
+    for(int i = 0; i < n; ++i){
+        bar[i] = i == 0? 0 : rand()%200;
+        cout << bar[i] << "|";
+    }
+    int minCost = getMinCost(bar, n, 0);
+    cout << "\nLowest cost is: " << minCost ;
     return 0;
 }
